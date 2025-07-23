1b91b727aaa88593895171a24ed15e8a504af502
xiaoDeng <524846102@qq.com>
2025-07-24 00:10:48 +0800

    initial

--- src/main/java/solution/Solution647.java
+++ src/main/java/solution/Solution647.java
@@ -2,6 +2,18 @@ package solution;
 
 public class Solution647 {
     public int countSubstrings(String s) {
+        /**
+         * 暴力解法，两个步骤
+         * 1. 找出所有的子串
+         * 1.1 i为选择的长度，1,2,3
+         * 1.2 第二个循环j = 0; j + i < s.length, 截取 [j,j+i]的字符串，执行第二步判断
+         * 2. 对所有的字串判断是否是回文字符串
+         *
+         */
+        StringBuilder stringBuilder = new StringBuilder(s);
 
+        for (int i = 0; i < s.length(); i++) {
+
+        }
     }
 }
